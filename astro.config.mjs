3875dd70475f316ba31e394b88c616658ad455be
Tristan Gruener <tristang@stingraydirect.com>
2026-07-08 12:24:50 -0700

    init

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+    }),
+  ],
+});
