fae593a328395cae6304e50dd1304abf12e68376
Tristan Gruener <tristang@stingraydirect.com>
2026-07-21 21:29:07 -0700

    prep for vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 import { defineConfig } from 'astro/config';
 import { storyblok } from '@storyblok/astro';
 import { loadEnv } from 'vite';
+import vercel from '@astrojs/vercel';
 import mkcert from 'vite-plugin-mkcert';
 
 const env = loadEnv('', process.cwd(), 'STORYBLOK');
+const isDev = process.argv.includes('dev');
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
+      accessToken: env.STORYBLOK_TOKEN || process.env.STORYBLOK_TOKEN,
+      livePreview: true,
     }),
   ],
   vite: {
-    plugins: [mkcert()],
+    plugins: isDev ? [mkcert()] : [],
   },
 });
